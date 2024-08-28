2954f60eeb683accfa787ed9980023574ee2c463
Levart9999 <levart9999@gmail.com>
2024-08-28 11:10:54 +0300

    comm 14

--- index.php
+++ index.php
@@ -9,16 +9,29 @@ elseif ($hour>=12 and $hour<18)
        $welcome = "Good afternoon";
 elseif ($hour>=18 and $hour<23)
        $welcome = "Good evening";
+
+set_error_handler("myError");
+
+
+
     ?>
 
 
 <html>
 <head>
-    <title>WOW</title>
+    <title>Edu</title>
+    <meta charset="utf-8"/>
 </head>
 <body>
+
 <blockquote>
     <h1 align="center"><?= $welcome ?></h1>
+    <!--Menu-->
+    <h2>What do you like to usage?</h2>
+    <?php
+    include "inc/lib.inc.php";
+    include "inc/menu.inc.php";
+    ?>
 
 </body>
 </html>
